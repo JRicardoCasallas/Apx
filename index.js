a626544d2c6e7b265073ddda56bda850ca395a7c
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-10-24 20:59:24 -0500

    proyecto APX

---
+++ index.js
@@ -0,0 +1,10 @@
+// diametro sería el input
+const diametro = 5;
+
+const radio = diametro / 2;
+const radioAlCuadrado = radio * radio;
+const area = Math.PI * radioAlCuadrado;
+
+
+// area sería el output
+console.log("area de la circunferencia", area);
